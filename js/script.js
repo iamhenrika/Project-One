b592534a0073f751a5b175783255dc20c6a36949
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-11-07 12:27:31 -0500

    Updated.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let sequence = [];
+let humanSequence = [];
+//keep track of the tile clicks
+let level = 0;
+
+const startButton = document.querySelector('.js-start');
+const info = document.querySelector('.js-info');
+const heading = document.querySelector('.js-heading');
+const tileContainer = document.querySelector('.js-container');
