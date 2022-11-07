cec9784ad0fbc7d44923db039ddeb0916b2813ae
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2022-11-07 12:28:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 const heading = document.querySelector('.js-heading');
 const tileContainer = document.querySelector('.js-container');
+
+function resetGame(text) {
+    alert(text);
+    sequence = [];
+    humanSequence = [];
+    level = 0;
+    startButton.classList.remove('hidden');
+    heading.textContent = 'SIMON';
+    info.classList.add('hidden');
+    tileContainer.classList.add('unclickable');
+}
